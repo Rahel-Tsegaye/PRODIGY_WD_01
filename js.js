c5bcc7cb685271757b9cc31556ba4250a1706791
Rahel-Tsegaye <rahel25tsegaye@gmail.com>
2025-04-25 18:23:24 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+// js.js
+const nav = document.getElementById("nav-menu");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
